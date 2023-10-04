fc254a271fa55aad8878b42dd6950d85c8f8f50f
Rong-qian <qianron1@msu.edu>
2023-10-04 17:10:24 -0400

    create xadc_dual branch

---
+++ Plots/Readme.md
@@ -0,0 +1,43 @@
+# SMDT_FPGA xadc_dual Branch
+This branch contains the code for the dual channel xadc readout by Basys3.
+
+## Prerequisites
+If you are not familiar with FPGA, The tutorial branch is here for veiw.
+
+## Running
+1. Clone the git repository to /your/path.
+ ```
+git clone https://github.com/Rong-qian/sMDT_FPGA.git
+git checkout xadc_dual
+```
+2. Open vivado and open the XADC_DUAL.xpr under XADC_DUAL folder.
+
+![avatar](../Plots/Open.PNG)
+
+3. Run Synthesis and Run Implementation (click red-lined butoon in the left panel)
+
+![avatar](../Plots/SI.PNG)
+
+4. Connect the fpga board as shown below. The uart should be connect to PC. In the illustration Two inputs are from the wavegenerator and is connect to JA PMOD connector. Two colomns are two channels and the positive should be connected to the upper connector. Detail see: https://digilent.com/reference/programmable-logic/basys-3/demos/xadc
+
+![avatar](../Plots/Connect.jpeg)
+
+5. Make Sure JP1 is in JTAG mode and Turn on the Basys3. Generate Bitstream and program device. Click the botton as shown in the picture. 
+
+![avatar](../Plots/Hardware.PNG)
+
+6.The input signal should be between 0-1V. The LED[0-7] and LED[8-15] will shows the bit of two channel ADC.  
+
+## Material 
+
+1. https://www.unilim.fr/pages_perso/vahid.meghdadi-neyshabouri/XADCinBasys3.html
+
+    The code is based on this website. See the youtube video inside it for more detail if you want to know how to use XADC.
+
+2. https://docs.xilinx.com/r/en-US/ug480_7Series_XADC/Instantiating-the-XADC
+
+    Official document of XADC by Xilinx.
+
+## Next step
+
+1. Add uart output to show results and save in PC. 
---
+++ XADC_DUAL/XADC_DUAL.ip_user_files/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl
@@ -0,0 +1,48 @@
+-- Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+-- Copyright 2022-2023 Advanced Micro Devices, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2023.1 (win64) Build 3865809 Sun May  7 15:05:29 MDT 2023
+-- Date        : Mon Oct  2 17:10:41 2023
+-- Host        : DESKTOP-V8TE6SP running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/smdt/XADC_DUAL/XADC_DUAL.gen/sources_1/ip/xadc_wiz_0/xadc_wiz_0_stub.vhdl
+-- Design      : xadc_wiz_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7a35tcpg236-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity xadc_wiz_0 is
+  Port ( 
+    daddr_in : in STD_LOGIC_VECTOR ( 6 downto 0 );
+    den_in : in STD_LOGIC;
+    di_in : in STD_LOGIC_VECTOR ( 15 downto 0 );
+    dwe_in : in STD_LOGIC;
+    do_out : out STD_LOGIC_VECTOR ( 15 downto 0 );
+    drdy_out : out STD_LOGIC;
+    dclk_in : in STD_LOGIC;
+    reset_in : in STD_LOGIC;
+    convst_in : in STD_LOGIC;
+    vauxp4 : in STD_LOGIC;
+    vauxn4 : in STD_LOGIC;
+    vauxp12 : in STD_LOGIC;
+    vauxn12 : in STD_LOGIC;
+    busy_out : out STD_LOGIC;
+    channel_out : out STD_LOGIC_VECTOR ( 4 downto 0 );
+    eoc_out : out STD_LOGIC;
+    eos_out : out STD_LOGIC;
+    alarm_out : out STD_LOGIC;
+    vp_in : in STD_LOGIC;
+    vn_in : in STD_LOGIC
+  );
+
+end xadc_wiz_0;
+
+architecture stub of xadc_wiz_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "daddr_in[6:0],den_in,di_in[15:0],dwe_in,do_out[15:0],drdy_out,dclk_in,reset_in,convst_in,vauxp4,vauxn4,vauxp12,vauxn12,busy_out,channel_out[4:0],eoc_out,eos_out,alarm_out,vp_in,vn_in";
+begin
+end;
