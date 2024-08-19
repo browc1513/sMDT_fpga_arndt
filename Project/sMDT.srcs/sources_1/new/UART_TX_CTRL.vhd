3d9414a8b124747e2bd778355afbca1fe8fff4b9
arndtale <143982050+arndtale@users.noreply.github.com>
2024-08-19 15:09:51 -0400

    8/19 Morning Update

--- Project/sMDT.srcs/sources_1/new/UART_TX_CTRL.vhd
+++ Project/sMDT.srcs/sources_1/new/UART_TX_CTRL.vhd
@@ -51,7 +51,7 @@ architecture Behavioral of UART_TX_CTRL is
 
 type TX_STATE_TYPE is (RDY, LOAD_BIT, SEND_BIT);
 
-constant BIT_TMR_MAX : std_logic_vector(13 downto 0) := "10100010110001"; --10416 = (round(100MHz / 9600)) - 1
+constant BIT_TMR_MAX : std_logic_vector(13 downto 0) := "10100010110001"; --10417 = (round(100MHz / 9600)) 
 constant BIT_INDEX_MAX : natural := 10; -- no longer than 10 bits, (8 data + 2 start/stop bits)
 
 --Counter that keeps track of the number of clock cycles the current bit has been held stable over the
